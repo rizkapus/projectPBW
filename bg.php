4bd9f8fac0791ab2f8302342ac14402973c667d7
Rizka Puspitasari <rizkapspitas@gmail.com>
2017-04-07 20:08:41 +0700

    Create bg.php

---
+++ bg.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<style>
+
+		body{
+		background-image: url("img/buildings.jpg");
+		background-size: 100%;
+		}
+
+		@keyframes changing{
+			from{
+				background: rgb(0, 0, 0, 0.5);
+				color: white;
+				box-shadow: 1px 1px 50px 0.5px white;
+			}
+			to { 
+				background: rgba(255, 255, 255, 0.5);
+				color: black;
+				box-shadow: 1px 1px 50px 0.5px black;
+			}
+		}
+	</style>
+</body>
+</html>
